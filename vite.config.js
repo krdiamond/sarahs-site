4d4882dc42a2efe3ca1f0f5fadbabc99e3f89ff5
Kristina Diamond <krdiamond@gmail.com>
2026-09-14 21:58:50 -0400

    Rename project to sarahs-site.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// Project Pages URL is https://krdiamond.github.io/portfolio/
+// Project Pages URL is https://krdiamond.github.io/sarahs-site/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/sarahs-site/',
   plugins: [vue(), tailwindcss()],
 })
